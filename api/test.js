67846fb317c3d52707ac3b85cb77ffa852c3e2a5
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2020-11-27 18:01:17 +0530

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,8 +1,17 @@
+var ip;
+if (req.headers['x-forwarded-for']) {
+    ip = req.headers['x-forwarded-for'].split(",")[0];
+} else if (req.connection && req.connection.remoteAddress) {
+    ip = req.connection.remoteAddress;
+} else {
+    ip = req.ip;
+}
+
 module.exports = (req, res) => {
   res.json({
     body: req.body,
     query: req.query,
     cookies: req.cookies,
-    ip: req.connection.socket,
+    ip: ip,
   })
 }
