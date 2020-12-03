55be0253626d5b7c4a23db1a896092acc692c310
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2020-12-03 19:28:05 +0530

    Update append.js

--- api/append.js
+++ api/append.js
@@ -1,10 +1,24 @@
 module.exports = async (req, res) => {
-const fetch = require('node-fetch');
+var https = require('https');
+
+/**
+ * HOW TO Make an HTTP Call - GET
+ */
+// options for GET
+var optionsget = {
+    host : 'script.google.com', // here only the domain name
+    port : 443,
+    path : '/macros/s/AKfycbznvGSPRcCSI92QKM-Ol8nwnooXgW7zkwkzDveCIFwdqcxgMKgp/exec?shid=1wa3RIo6kbNXiL03qUxp-HQO8OOQFMRZYY3UVL_2Pq_c&sn=Sheet1&d1=John&d2=39', // the rest of the url with parameters if needed
+    method : 'GET' // do GET
+};
 
 
-var response = fetch("https://script.google.com/macros/s/AKfycbznvGSPRcCSI92QKM-Ol8nwnooXgW7zkwkzDveCIFwdqcxgMKgp/exec?shid=1wa3RIo6kbNXiL03qUxp-HQO8OOQFMRZYY3UVL_2Pq_c&sn=Sheet1&d1=John&d2=39");
-var data = response.body();
+// do the GET request
+var reqGet = https.request(optionsget, function(res) {
 res.send({"done": "true"});
+});
+
+
 }
 
 
