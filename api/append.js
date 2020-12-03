538b7eaf9911fdcf31ef35eec95287a158e754b0
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2020-12-03 19:02:59 +0530

    Update append.js

--- api/append.js
+++ api/append.js
@@ -2,9 +2,9 @@ module.exports = async (req, res) => {
 const fetch = require('node-fetch');
 
 
-var response = fetch("https://example.com");
+var response = fetch("https://script.google.com/macros/s/AKfycbznvGSPRcCSI92QKM-Ol8nwnooXgW7zkwkzDveCIFwdqcxgMKgp/exec?shid=1wa3RIo6kbNXiL03qUxp-HQO8OOQFMRZYY3UVL_2Pq_c&sn=Sheet1&d1=John&d2=39");
 var data = response.body();
-res.send(data);
+res.send({"done": "true"});
 }
 
 
